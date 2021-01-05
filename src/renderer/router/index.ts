5300d3e55a3f24fde3fab02291bcda1820c7ab24
biantao <biantao@360.cn>
2021-01-05 13:15:57 +0800

    feat:新增欢迎页

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -2,8 +2,14 @@ import {createRouter, createWebHashHistory} from "vue-router";
 
 export default createRouter({
     history: createWebHashHistory(),
-    routes: [{
-        path: "/",
+    routes: [
+      {
+        path:'/',
+        name:'Welcome',
+        component:()=>import('../views/pages/Welcome.vue')
+      },
+      {
+        path: "/home",
         name: "Home",
         component: () => import(/* webpackChunkName: "home" */ "../views/pages/Home.vue")
     }]
