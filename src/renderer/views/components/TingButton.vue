d6d84e6d71280268091cadc9ab182285d06ea74c
biantao <biantao@360.cn>
2021-01-05 13:15:41 +0800

    feat:新增公用组件button

---
+++ src/renderer/views/components/TingButton.vue
@@ -0,0 +1,30 @@
+<style lang="scss" scoped>
+button {
+  width: 120px;
+  height: 32px;
+  margin-top: 35px;
+  background-color: #000;
+  outline: none;
+  color: #ffffff;
+  font-size: 17px;
+}
+</style>
+<template>
+  <button>{{ text }}</button>
+</template>
+<script>
+export default {
+  name: 'TingButton',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      text: props.text,
+    }
+  },
+}
+</script>
